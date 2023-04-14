707ae4a4777f35109f27b5264e886be476d09c1f
Alireza Safaierad <frontendmonster@gmail.com>
2023-04-14 13:48:01 +0400

    extract xdg config

--- modules/home.nix
+++ modules/home.nix
@@ -1,42 +1,9 @@
 { config, pkgs, inputs, hypr-contrib, user, ... }:
 
-let
-  zsh-skill-theme = builtins.fetchurl {
-    url = "https://raw.githubusercontent.com/ASafaeirad/oh-my-zsh-skill-theme/main/skill.zsh-theme";
-    sha256 = "1x2l0mws3bm9gv7yrd5vvf3rs0gbynfxd39zffs99n1lbzc8w7b8";
-  };
-  customDir = pkgs.stdenv.mkDerivation {
-    name = "oh-my-zsh-custom-dir";
-    phases = [ "buildPhase" ];
-    buildPhase = ''
-      mkdir -p $out/themes
-      cp ${zsh-skill-theme} $out/themes/skill.zsh-theme
-    '';
-  };
-in
 {
   programs = {
     home-manager.enable = true;
     neovim.enable = true;
-
-    zsh = {
-      enable = true;
-      dotDir = ".config/zsh";
-      autocd = true;
-      history = {
-        path = "$ZDOTDIR/.zsh_history";
-        save = 100000;
-        size = 100000;
-      };
-      oh-my-zsh = {
-        enable = true;
-        theme = "skill";
-        custom = "${customDir}";
-      };
-      historySubstringSearch = {
-        enable = true;
-      };
-    };
     vscode.enable = true;
   };
 
@@ -50,36 +17,7 @@ in
     network-manager-applet.enable = true;
   };
 
-  xdg = {
-    enable = false;
-    mimeApps = {
-      enable = true;
-      defaultApplications = {
-        "x-scheme-handler/http" = "brave-browser.desktop";
-        "x-scheme-handler/https" = "brave-browser.desktop";
-        "x-scheme-handler/notion" = "notion-app.desktop";
-        "x-scheme-handler/webcal" = "brave-browser.desktop";
-        "x-scheme-handler/tg" = "userapp-Telegram Desktop-V9RNW1.desktop";
-        "x-scheme-handler/mailto" = "branve-browser.desktop";
-        "text/plain" = "code.desktop";
-        "x-scheme-handler/postman" = "Postman.desktop";
-        "inode/directory" = "pcmanfm.desktop;ranger.desktop";
-        "application/json" = "code.desktop";
-      };
-    };
-
-    userDirs = {
-      enable = true;
-      desktop = "${config.home.homeDirectory}/desktop";
-      documents = "${config.home.homeDirectory}/docs";
-      download = "${config.home.homeDirectory}/download";
-      music = "${config.home.homeDirectory}/music";
-      pictures = "${config.home.homeDirectory}/pics";
-      videos = "${config.home.homeDirectory}/videos";
-    };
-  };
-
-  home = {
+   home = {
     stateVersion = "22.11";
     username = user;
     homeDirectory = "/home/${user}";
@@ -145,7 +83,6 @@ in
       EDITOR = "nvim";
       BROWSER = "brave";
       TERMINAL = "alacritty";
-      ZDOTDIR = "${config.xdg.configHome}/zsh";
       GNUPGHOME = "${config.xdg.dataHome}/gnupg";
       LESSHISTFILE = "/dev/null";
       WGETRC = "${config.xdg.configHome}/wget/wgetrc";
@@ -153,9 +90,6 @@ in
       KODI_DATA = "${config.xdg.dataHome}/kodi";
       PASSWORD_STORE_DIR = "${config.xdg.dataHome}/password-store";
       ANDROID_SDK_HOME = "${config.xdg.configHome}/android";
-      CARGO_HOME = "${config.xdg.dataHome}/cargo";
-      RUSTUP_HOME = "${config.xdg.dataHome}/rustup";
-      GOPATH = "${config.xdg.dataHome}/go";
       ANSIBLE_CONFIG = "${config.xdg.configHome}/ansible/ansible.cfg";
       UNISON = "${config.xdg.dataHome}/unison";
       HISTFILE = "${config.xdg.dataHome}/history";
---
+++ modules/xdg.nix
@@ -0,0 +1,39 @@
+{ config, pkgs, ... }:
+
+{
+  xdg = {
+    enable = false;
+    mimeApps = {
+      enable = true;
+      defaultApplications = {
+        "x-scheme-handler/http" = "brave-browser.desktop";
+        "x-scheme-handler/https" = "brave-browser.desktop";
+        "x-scheme-handler/notion" = "notion-app.desktop";
+        "x-scheme-handler/webcal" = "brave-browser.desktop";
+        "x-scheme-handler/tg" = "userapp-Telegram Desktop-V9RNW1.desktop";
+        "x-scheme-handler/mailto" = "branve-browser.desktop";
+        "text/plain" = "code.desktop";
+        "x-scheme-handler/postman" = "Postman.desktop";
+        "inode/directory" = "pcmanfm.desktop;ranger.desktop";
+        "application/json" = "code.desktop";
+      };
+    };
+
+    userDirs = {
+      enable = true;
+      desktop = "${config.home.homeDirectory}/desktop";
+      documents = "${config.home.homeDirectory}/docs";
+      download = "${config.home.homeDirectory}/download";
+      music = "${config.home.homeDirectory}/music";
+      pictures = "${config.home.homeDirectory}/pics";
+      videos = "${config.home.homeDirectory}/videos";
+    };
+  };
+
+  sessionVariables = {
+    XDG_CACHE_HOME = "$HOME/.cache";
+    XDG_CONFIG_HOME = "$HOME/.config";
+    XDG_DATA_HOME = "$HOME/.local/share";
+    XDG_BIN_HOME = "$HOME/.local/bin";
+  };
+}
