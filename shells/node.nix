2743210c11d7b7321f40b8d83fba8e6b8e1f3a88
Alireza Safaierad <frontendmonster@gmail.com>
2023-04-15 14:21:52 +0400

    add nodejs

--- shells/node.nix
+++ shells/node.nix
@@ -5,10 +5,14 @@ mkShell {
   buildInputs = [
     nodejs
     yarn
+    nodePackages.pnpm
+    nodePackages."@antfu/ni"
   ];
   shellHook = ''
     export NPM_CONFIG_CACHE="$XDG_CACHE_HOME/npm"
     export NPM_CACHE_PREFIX="$XDG_CACHE_HOME/npm"
     export PATH="$(yarn global bin):$PATH"
+    alias npms="nr start"
+    zsh
   '';
 }
