c1baef1e2c8f5a5187bc252433bafec3df05b4c3
Alireza Safaierad <frontendmonster@gmail.com>
2023-04-16 14:39:13 +0400

    add QT_QPA_PLATFORMTHEME

--- modules/hyprland.nix
+++ modules/hyprland.nix
@@ -23,6 +23,7 @@
     QT_QPA_PLATFORM = "wayland;xcb";
     QT_WAYLAND_DISABLE_WINDOWDECORATION = "1";
     QT_AUTO_SCREEN_SCALE_FACTOR = "1";
+    QT_QPA_PLATFORMTHEME= "qt5ct";
     WLR_DRM_DEVICES = "/dev/dri/card0";
     GDK_BACKEND = "wayland,x11";
     CLUTTER_BACKEND = "wayland";
