ec9832c5361e25cf877791fdd74bb67e5531676d
Alireza Safaierad <frontendmonster@gmail.com>
2023-04-24 20:50:13 +0400

    fix touchpad delay

--- hosts/common/awesome.nix
+++ hosts/common/awesome.nix
@@ -19,7 +19,11 @@
     layout = "us,ir";
     xkbOptions = "grp:shifts_toggle,caps:escape";
     displayManager.startx.enable = true;
-    libinput.enable = true;
+    libinput = {
+      enable = true;
+      touchpad.tappingDragLock = false;
+    };
+
     # disable automatic screen blanking and stuff, we'll do it manually instead
     serverFlagsSection = ''
       Option "BlankTime" "0"
@@ -28,5 +32,4 @@
       Option "OffTime" "0"
     '';
   };
-
 }
